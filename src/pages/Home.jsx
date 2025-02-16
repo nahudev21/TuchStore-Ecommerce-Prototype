efc72017506755c388511ba75971b7d96f20f2d6
Nahuel de la Torre <nahueldelatorre16@gmail.com>
2025-02-16 20:49:16 -0300

    Creación de banner para desktop y tablet

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import CategoryList from "../components/CategoryList";
+import ProductBanner from "../components/ProductBanner";
 
 export default function Home() {
   return (
     <div>
       <CategoryList />
+      <ProductBanner />
     </div>
   )
 }
