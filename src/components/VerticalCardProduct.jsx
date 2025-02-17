53d5f3e707fbcbdd14870f1e4a1d76e8bbfc5d48
Nahuel de la Torre <nahueldelatorre16@gmail.com>
2025-02-17 20:23:08 -0300

    Creación de componente de productos verticalmente

--- src/components/VerticalCardProduct.jsx
+++ src/components/VerticalCardProduct.jsx
@@ -4,11 +4,10 @@ import displayCurrency from "../helpers/displayCurrency";
 import { FaAngleLeft } from "react-icons/fa6";
 import { FaAngleRight } from "react-icons/fa6";
 
-export default function HorizontalproductsHome({ category, heading }) {
-
-  const [ data, setData ] = useState([]);
-  const [ loading, setLoading ] = useState(true);
-  const loadingList = new Array(13).fill(null);  
+export default function VerticalproductsHome({ category, heading }) {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const loadingList = new Array(13).fill(null);
 
   const [scroll, setScroll] = useState(0);
   const scrollElement = useRef();
@@ -16,7 +15,7 @@ export default function HorizontalproductsHome({ category, heading }) {
   const getProductsByCategory = async () => {
     setLoading(true);
     const res = await getProductsByCategoryRequest(category);
-    setLoading(false)
+    setLoading(false);
     setData(res?.data || []);
   };
 
@@ -26,17 +25,17 @@ export default function HorizontalproductsHome({ category, heading }) {
 
   const scrollRigth = () => {
     scrollElement.current.scrollLeft += 300;
-  }
+  };
 
   const scrollLeft = () => {
     scrollElement.current.scrollLeft -= 300;
-  }; 
+  };
 
   return (
     <div className="container mx-auto px-4 py-2 my-4 relative">
       <h2 className="text-2xl font-semibold ">{heading}</h2>
       <div
-        className="flex items-center gap-4 md:gap-6 my-4 overflow-scroll scrollbar-none transition-all"
+        className="flex items-center gap-4 md:gap-6 my-4 overflow-x-scroll scrollbar-none transition-all"
         ref={scrollElement}
       >
         <button
@@ -52,26 +51,30 @@ export default function HorizontalproductsHome({ category, heading }) {
           <FaAngleRight className="hover:scale-110" />
         </button>
         {loading ? (
-          <p className="">Cargando...</p>
+          <p>Cargando...</p>
         ) : (
           data.map((product) => {
             return (
               <div
                 key={product.id}
-                className="h-36 min-w-[280px] md:min-w-[320px] max-w-[280px] md:max-w-[320px] bg-white rounded-sm shadow flex"
+                className="min-w-[280px] md:min-w-[320px] max-w-[280px] md:max-w-[320px] bg-white rounded-sm shadow "
               >
-                <div className="h-full bg-slate-200 p-4 min-w-[120px] md:min-w-[145px] ">
+                <div className="h-48 bg-slate-200 p-4 min-w-[280px] md:min-w-[145px] flex items-center justify-center">
                   <img
                     src={product.images[0]}
                     alt={"Imagen" + product?.name}
-                    className="w-full h-full object-contain mix-blend-multiply hover:scale-125 transition-all"
+                    className="w-full h-full object-contain mix-blend-multiply hover:scale-110 transition-all"
                   />
                 </div>
-                <div className="p-2 grid">
-                  <h2 className="font-medium text-[14px] md:text-[16px] line-clamp-1">
-                    {product?.name}
-                  </h2>
-                  <p className="capitalize text-black">{product?.category}</p>
+                <div className="p-2 grid gap-2">
+                  <div>
+                    <h2 className="font-medium text-[14px] md:text-[16px] line-clamp-1">
+                      {product?.name}
+                    </h2>
+                    <span className="block text-[13px] text-[#4189e6b7] font-medium ">
+                      Estado {product.status}
+                    </span>
+                  </div>
                   <div className="flex flex-col">
                     <p className="text-red-400 line-through text-[15px]">
                       {displayCurrency(product?.price)}
